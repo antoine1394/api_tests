38058336cb7538f434391afcb764ad1e8f11e039
antoine1394 <anton.ivashuk@gmail.com>
2022-08-09 12:42:25 +0300

    updated api tests

--- GET_requests/GetRequest_with_parameters.py
+++
@@ -1,8 +0,0 @@
-import requests
-parameters = {
-    'name': 'testingAPI',
-    'email': 'testing@i.ua',
-    'number': '+380991234567'
-}
-response = requests.get('https://httpbin.org/get', params=parameters)
-print(response.text)
--- GET_requests/delete_user.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-# API URL
-url = "https://reqres.in/api/users/2"
-response = requests.delete(url)
-# fetch response code
-print(response.status_code)
-assert response.status_code == 204
